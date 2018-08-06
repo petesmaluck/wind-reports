f4a30223790f073b81d04c141263883d032aa094
Pete Smaluck <petesmaluck@gmail.com>
2018-08-06 19:01:42 -0400

    Added template, controller, css

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,3 +25,16 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to metar-reports!');
   }));
 });
+
+// At minimum we require an interface that lets us:
+
+// parse a METAR format record as described above
+// the returned data structure should normalize speeds to MPS
+// you can normalize a speed value in KT by dividing the value by 2
+// Read a stream of records, one per line
+// and keep a running average wind speed per airport seen
+// and the current wind speed of each airport seen
+// Write a script to generate a few hundred thousand random reports one per line. Your program should be able to read the entire stream without failure and be able to either:
+
+// display the running tally
+// print a final report after reading the entire stream
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DataGeneratorService } from './services/data-generator.service';
+import { DataParserService } from './services/data-parser.service';
+import { CalculationsService } from './services/calculations.service';
+
+import { Observable, fromEvent } from 'rxjs';
+import { mergeMap, tap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  metarReports: Observable<any>;
+  airportsFormattedDataArray = [];
+  airportDataByIcaoCode: Object;
+  objectKeys = Object.keys;
+  airportWindData: any;
+  dataPollInterval = 700;
+
+  constructor(
+    private dataGenerator: DataGeneratorService,
+    public dataParser: DataParserService,
+    public dataOperation: CalculationsService
+  ) {}
+
+  ngOnInit() {
+    // Initialize data
+    this.metarReports = this.dataGenerator.poll$(this.dataPollInterval);
+
+    // Stream data
+    this.metarReports
+      .pipe(
+        tap(res => this.airportsFormattedDataArray = []),
+        mergeMap(data => data),
+        map((res: string) => this.dataParser.formatData(res))
+      ).subscribe((res) => {
+
+        // Push to front of METAR data array
+        this.airportsFormattedDataArray.unshift(res);
+
+        // Group airports by icao_code
+        this.airportDataByIcaoCode = this.dataParser.groupDataByKey(this.airportsFormattedDataArray, 'icao_code');
+        
+        // Get list of airports
+        this.airportWindData = Object.keys(this.airportDataByIcaoCode);
+      });
+  }
 }
